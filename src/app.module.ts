63a3d5c2a71b2cfb4404015881c338cf6dda987a
minho <dnjsalsgh123@gmail.com>
2024-06-25 21:29:26 +0900

    [UPDATE]: set comment table

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from 'src/config/prisma/prisma.module';
 import { UserModule } from 'src/api/user/user.module';
 import { PostModule } from 'src/api/post/post.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, PostModule],
+  imports: [PrismaModule, UserModule, PostModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
