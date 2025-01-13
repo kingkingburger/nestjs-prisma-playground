7037ba4cc3d064cf123cf033f44c2c144860c66b
minho <dnjsalsgh123@gmail.com>
2025-01-13 18:50:46 +0900

    update: remove simplify domain that used only user, post, comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,6 @@ import { AppService } from './app.service';
 import { UserModule } from './api/user/user.module';
 import { PostModule } from './api/post/post.module';
 import { CommentModule } from './api/comment/comment.module';
-import { CategoryModule } from './api/category/category.module';
-import { NotificationModule } from './api/notification/notification.module';
-import { ActiveLogModule } from './api/active-log/active-log.module';
 import { PrismaModule } from './config/database/prisma.module';
 import { AuthModule } from './api/auth/auth.module';
 
@@ -16,9 +13,6 @@ import { AuthModule } from './api/auth/auth.module';
     UserModule,
     PostModule,
     CommentModule,
-    CategoryModule,
-    NotificationModule,
-    ActiveLogModule,
     AuthModule,
   ],
   controllers: [AppController],
