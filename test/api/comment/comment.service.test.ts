386fedb789bc275ccc8f903547667a67dbff810e
minho <dnjsalsgh123@gmail.com>
2025-11-09 09:45:12 +0900

    test: create comment

--- test/api/comment/comment.service.test.ts
+++ test/api/comment/comment.service.test.ts
@@ -1,7 +1,8 @@
 import { PrismaService } from '../../../src/config/database/prisma.service';
-import { Post, User } from '@prisma/client';
+import { Comment, Post, User } from '@prisma/client';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentService } from '../../../src/api/comment/comment.service';
+import { CreateCommentDto } from '../../../src/api/comment/dto/create-comment.dto';
 
 describe('CommentService', () => {
   let service: CommentService;
@@ -14,6 +15,12 @@ describe('CommentService', () => {
       findMany: jest.fn(),
       count: jest.fn(),
       create: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+    comment: {
+      create: jest.fn(),
+      findMany: jest.fn(),
       update: jest.fn(),
       delete: jest.fn(),
     },
@@ -43,6 +50,16 @@ describe('CommentService', () => {
     recommendCount: 0,
   };
 
+  const mockComment: Comment = {
+    id: 1,
+    content: 'Test Comment',
+    userId: 1,
+    postId: 1,
+    parentId: null,
+    createdAt: new Date('2025-11-01'),
+    updatedAt: new Date('2025-11-01'),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -62,5 +79,28 @@ describe('CommentService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createComment', () => {
+    test('should create a new Comment', async () => {
+      const createCommentDto: CreateCommentDto = {
+        content: 'Test Comment',
+        userId: 1,
+        postId: 1,
+      };
+
+      mockPrismaService.comment.create.mockResolvedValue(mockComment);
+
+      const result = await service.createComment(createCommentDto);
+
+      expect(mockPrismaService.comment.create).toHaveBeenCalledWith({
+        data: {
+          content: 'Test Comment',
+          User: { connect: { id: 1 } },
+          Post: { connect: { id: 1 } },
+        },
+      });
+      expect(result).toEqual(mockComment);
+    });
   });
 });
